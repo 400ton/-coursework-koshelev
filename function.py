8b9a17a665976c4bdf25661a793c26211459eb05
Tony <king8sky@gmail.com>
2024-02-05 23:47:41 +1100

    fix name

--- function.py
+++ function.py
@@ -3,14 +3,14 @@ import os.path
 
 
 def load_file(filename) -> list:
-    '''загружает информацию из файла'''
+    '''Функция загрузки информации из файла'''
     file_path = os.path.join(filename)
     with open(file_path, 'r', encoding='utf-8') as file:
         return json.load(file)
 
 
 def sort_operations(operations):
-    """Фильтрует операции по дате"""
+    """Функция фильтрации операции по дате"""
     if not operations:
         return []
     return sorted(operations, key=lambda operation: operation.get('date', '0000-00-00'), reverse=True)
@@ -24,7 +24,7 @@ def filter_operations(operations):
 
 
 def formatter_date(date):
-    '''Форматируем дату'''
+    '''Функция форматирования даты'''
     date = date.split('T')[0].split('-')
     return f'{date[2]}.{date[1]}.{date[0]}'
 
@@ -39,9 +39,9 @@ def encryption(data, key):
     number = value_number[-1]
 
     if key == "from":
-        encryption = number[0: 4] + ' ' + number[4: 6]+ '** **** ' + number[-4:]
+        encryption_key = number[0: 4] + ' ' + number[4: 6]+ '** **** ' + number[-4:]
     elif key == "to":
-        encryption = '**' + number[-4:]
-    return ' '.join([*value, encryption])
+        encryption_key = '**' + number[-4:]
+    return ' '.join([*value, encryption_key])
 
 

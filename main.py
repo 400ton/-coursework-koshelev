fbe7b64435b56bffad1fa351dab99afbfecd89f6
Tony <king8sky@gmail.com>
2024-02-06 20:52:22 +1100

    project 1.1.0
    code review, adding testing functions

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ def main():
     operations = load_file("data/operations.json")
 
     #Сортируем операции по дате
-    sort_data = sort_operations(operations)
+    sort_data = sort_operations(operations, 'date')
 
     #Фильтруем операции по статусу ВЫПОЛНЕННО
-    filter_state = filter_operations(sort_data)
+    filter_state = filter_operations(sort_data, 'state', 'EXECUTED')
 
     #Обрезаем первые пять выполненных операций
     five_operations = filter_state[:5]
-    for operations in five_operations:
-        if 'from' not in operations:
-            operations['from'] = ''
+    for operation in five_operations:
+        if 'from' not in operation:
+            operation['from'] = ''
 
-        print(f'{formatter_date(operations["date"])} {operations["description"]}\n'
-              f'{encryption(operations["from"], "from")} -> {encryption(operations["to"], "to")}'
-              f' {operations['operationAmount']['amount']} {operations['operationAmount']["currency"]["name"]}\n')
+        print(f'{formatter_date(operation["date"])} {operation["description"]}\n'
+              f'{encryption(operation["from"], "from")} -> {encryption(operation["to"], "to")}'
+              f' -> {operation['operationAmount']['amount']} {operation['operationAmount']["currency"]["name"]}\n')
 
 if __name__ == '__main__':
     main()

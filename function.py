fb708ce14ac52340e17f5d189778b9863b5338ae
Tony <king8sky@gmail.com>
2024-02-06 20:53:12 +1100

    project 1.1.0
    code review, added testing function, updated dependencies

--- function.py
+++ function.py
@@ -6,7 +6,7 @@ def load_file(filename) -> list:
     '''Функция загрузки информации из файла'''
     file_path = os.path.join(filename)
     with open(file_path, 'r', encoding='utf-8') as file:
-        return json.load(file)
+        return json.load(fil)
 
 
 def sort_operations(operations, key):
